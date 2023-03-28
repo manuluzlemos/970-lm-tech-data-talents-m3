c9aca09015cad85654adee4c4fae6e101c8f4fa4
Emanuelle Lemos <manuluz.lemos@gmail.com>
2023-03-27 23:14:44 -0300

    fix: mensagem do TypeError

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,16 +1,18 @@
 def soma(a, b):
     if not (type(a) in (int, float) and type(b) in (int, float)):
-        raise TypeError()
+        raise TypeError(f'As entradas "a" e "b" devem ser tipos numéricos. Recebeu-se a = {a} ({type(a)}) e b = {b} ({type(b)})')
+    
     return a+b
 
 def subtracao(a, b):
     if not (type(a) in (int, float) and type(b) in (int, float)):
-        raise TypeError()
+        raise TypeError(f'As entradas "a" e "b" devem ser tipos numéricos. Recebeu-se a = {a} ({type(a)}) e b = {b} ({type(b)})')
+
     return a-b
 
 def divisao(a, b):
     if not (type(a) in (int, float) and type(b) in (int, float)):
-        raise TypeError()
+        raise TypeError(f'As entradas "a" e "b" devem ser tipos numéricos. Recebeu-se a = {a} ({type(a)}) e b = {b} ({type(b)})')
     
     if b == 0:
         print("Divisão inválida")
@@ -20,5 +22,5 @@ def divisao(a, b):
 
 def multiplicao(a, b):
     if not (type(a) in (int, float) and type(b) in (int, float)):
-        raise TypeError()
+        raise TypeError(f'As entradas "a" e "b" devem ser tipos numéricos. Recebeu-se a = {a} ({type(a)}) e b = {b} ({type(b)})')
     return a*b

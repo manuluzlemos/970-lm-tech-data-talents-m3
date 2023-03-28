59f949ec74e59ece50a16671853e69956b84f9d8
Emanuelle Lemos <manuluz.lemos@gmail.com>
2023-03-27 23:04:59 -0300

    feat: subtracao

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -2,3 +2,8 @@ def soma(a, b):
     if not (type(a) in (int, float) and type(b) in (int, float)):
         raise TypeError()
     return a+b
+
+def subtracao(a, b):
+    if not (type(a) in (int, float) and type(b) in (int, float)):
+        raise TypeError()
+    return a-b

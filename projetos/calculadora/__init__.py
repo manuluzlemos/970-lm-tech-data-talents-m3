0fb09961905b49f03f8a225e25aa687cdf15919d
Emanuelle Lemos <manuluz.lemos@gmail.com>
2023-03-29 20:39:25 -0300

    fix: opções dos símbolos como entrada para a operação de cálculo

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,18 +1,16 @@
 from funcoes import *
 
-funcoes = {
-    "soma": soma,
-    "subtracao": subtracao,
-    "divisao": divisao,
-    "multiplicacao": multiplicacao
-}
+funcoes = (soma, subtracao, divisao, multiplicacao)
+op_extenso = ("soma", "subtracao", "divisao", "multiplicacao")
+op_simbolos = ("+", "-", "/", "*")
 
 def calcule():
     a = float(input("Informe o primeiro número: "))
     b = float(input("Informe o segundo número: "))
-    op = input("Informe a operação (soma, subtracao, divisao, multiplicacao): ")
+    op = input("Informe a operação (soma, subtracao, divisao, multiplicacao, +, -, /, *): ")
 
-    if op in funcoes.keys():
-        print(f'Resultado da operação: {funcoes[op](a, b)}')
+    if op in op_extenso or op in op_simbolos:
+        index = op_extenso.index(op) if op in op_extenso else op_simbolos.index(op)    
+        print(f'Resultado da operação: {funcoes[index](a, b)}')
     else:
-        print("Operaração inválida!")
+        print("Operação inválida!")
